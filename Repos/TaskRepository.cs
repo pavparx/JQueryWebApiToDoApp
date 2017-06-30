d7070713a995b1d19bed4e44d237008d1fce9f38
Pavlos <pavlos@ascentsoftware.eu>
2017-06-30 11:17:16 +0200

    TaskRepository gets data with Linq from DB.

--- Repos/TaskRepository.cs
+++ Repos/TaskRepository.cs
@@ -11,21 +11,25 @@ namespace Repos
 {
     public class TaskRepository
     {
-        //DbContextClass Db = new DbContextClass();
+        
 
-        private static List<Models.Task> tasks = new List<Models.Task>
-        {
-            new Models.Task() {Id=1, Name="Task1", Description="Desc1", Done=false },
-            new Models.Task() {Id=2, Name="Task2", Description="Desc2", Done=false },
-            new Models.Task() {Id=3, Name="Task3", Description="Desc3", Done=false },
-        };
+        DbContextClass.DbAccess Db = new DbContextClass.DbAccess();
+
+        //private static List<Models.Task> query = new List<Models.Task>
+        //{
+        //    new Models.Task() {Id=1, Name="Task1", Description="Desc1", Done=false },
+        //    new Models.Task() {Id=2, Name="Task2", Description="Desc2", Done=false },
+        //    new Models.Task() {Id=3, Name="Task3", Description="Desc3", Done=false },
+        //};
         public List<Models.Task> GetTasks()
         {
 
-            //var query = from data in Db
-            //            orderby data.Id
-            //            select data;
-            return tasks;
+           IQueryable<Models.Task> query = from data in Db.Tasks
+                        orderby data.Id
+                        select data;
+
+            List<Models.Task> results = new List<Models.Task>(query);
+            return results;
         }
     }
 }
