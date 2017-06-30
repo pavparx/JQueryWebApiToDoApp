2a69be116023486211f9162da41f367efb026cc3
Pavlos <pavlos@ascentsoftware.eu>
2017-06-30 14:16:59 +0200

    Setting up solution structure for DI.

--- Repos/TaskRepository.cs
+++ Repos/TaskRepository.cs
@@ -11,22 +11,16 @@ namespace Repos
 {
     public class TaskRepository
     {
-        
+
 
         DbContextClass.DbAccess Db = new DbContextClass.DbAccess();
 
-        //private static List<Models.Task> query = new List<Models.Task>
-        //{
-        //    new Models.Task() {Id=1, Name="Task1", Description="Desc1", Done=false },
-        //    new Models.Task() {Id=2, Name="Task2", Description="Desc2", Done=false },
-        //    new Models.Task() {Id=3, Name="Task3", Description="Desc3", Done=false },
-        //};
         public List<Models.Task> GetTasks()
         {
 
-           IQueryable<Models.Task> query = from data in Db.Tasks
-                        orderby data.Id
-                        select data;
+            IQueryable<Models.Task> query = from data in Db.Tasks
+                                            orderby data.Id
+                                            select data;
 
             List<Models.Task> results = new List<Models.Task>(query);
             return results;
--- UnityBootstrapper/ContainerBootstrapper.cs
+++ UnityBootstrapper/ContainerBootstrapper.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Models;
 
-namespace IRepositories
+namespace UnityBootstrapper
 {
-    interface ITaskRepository
+    public class ContainerBootstrapper
     {
-
-        List<Models.Task> GetTasks();
-
     }
 }
