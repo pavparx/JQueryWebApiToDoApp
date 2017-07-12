ecb7923a430b5819b4e09099c6c6b875c2189aa3
Pavlos <pavlos@ascentsoftware.eu>
2017-07-12 16:41:44 +0200

    Added JQuery UI. TODO: Have separate results and filtered results.

--- JQueryWebApiDemo/JS/main.js
+++ JQueryWebApiDemo/JS/main.js
@@ -72,28 +72,26 @@
         }
 
         function displayTable(param) {
-
-
-            var $table = $("<table>");
-            $table.attr("border", "1");
+            var $table = $("<table />");
+            $table.attr("border", "1");  
             var $tbody = $("<tbody>");
             var $thead = $("<thead>");
 
             $thead.append($("<tr>").html("<td>ID</td><td>Creator Name</td><td>Name</td><td>Description</td><td>Completed</td>"));
             $table.append($thead);
 
-            for (var i = 0; i < param.length; i++) {
+            for (var i = 0; i < param.length; i++) { 
                 var $tr = $("<tr>");
-                $tdId = $("<td>" + param[i].id + "</td>");
-                $tdUserName = $("<td>" + param[i].userName + "</td>");
-                $tdName = $("<td>" + param[i].name + "</td>");
-                $tdUserDescription = $("<td>" + param[i].description + "</td>");
-                $tdDone = $("<td>" + param[i].done + "</td>");
-                $tr.append($tdId).append($tdUserName).append($tdName).append($tdUserDescription).append($tdDone);
+                $("<td />").text(param[i].id).appendTo($tr);
+                $("<td />").text(param[i].userName).appendTo($tr);
+                $("<td />").text(param[i].name).appendTo($tr);
+                $("<td />").text(param[i].description).appendTo($tr);
+                $("<td />").text(param[i].done).appendTo($tr);
                 $tbody.append($tr);
-                //append($tdUserName).append($tdName).append($tdUserDescription).append($tdDone);
             }
+
             $table.append($tbody);
+            
             $('#results').append($table);
 
             var results = $('#results');
@@ -102,52 +100,10 @@
 
         }
 
-        //// creating all cells
-        //for (var i = 0; i < param.length; i++) {
-
-        //        var row = document.createElement("tr");
-
-        //        var cell = document.createElement("td");
-        //        var cellText = document.createTextNode(param[i].id);
-        //        cell.appendChild(cellText);
-        //        row.appendChild(cell);
-
-        //        var cell1 = document.createElement("td");
-        //        var cellText1 = document.createTextNode(param[i].userName);
-        //        cell1.appendChild(cellText1);
-        //        row.appendChild(cell1);
-
-
-        //        var cell2 = document.createElement("td");
-        //        var cellText2 = document.createTextNode(param[i].name);
-        //        cell2.appendChild(cellText2);
-        //        row.appendChild(cell2);
-
-        //        var cell3 = document.createElement("td");
-        //        var cellText3 = document.createTextNode(param[i].description);
-        //        cell3.appendChild(cellText3);
-        //        row.appendChild(cell3);
-
-        //        var cell4 = document.createElement("td");
-        //        var cellText4 = document.createTextNode(param[i].done);
-        //        cell4.appendChild(cellText4);
-        //        row.appendChild(cell4);
-
-
-        //        tblBody.appendChild(row);
-
-        //        tbl.appendChild(tblBody);
-
-        //        var results = jQuery('#results');
-        //        results.empty();
-        //        results.append(tbl);
-
-        //        tbl.setAttribute("border", "2");
-
-
-
+        $(function () {
+            $("#tabs").tabs();
+        });
 
 
     });
-    
 })();
