6ffc18bdca4231df7bf85aa0b68a1d3e90f86d5c
Pavlos <pavlos@ascentsoftware.eu>
2017-07-12 17:33:57 +0200

    Latest

--- JQueryWebApiDemo/JS/main.js
+++ JQueryWebApiDemo/JS/main.js
@@ -46,23 +46,22 @@
             }, queryFailed);
 
         function querySucceeded(data) {
-
-            displayTable(data);
+            
+            
 
             $("#filterTasks").keyup(function () {
 
                 var tempArray = [];
 
                 var keyword = $(this).val();
-
-                for (var i = 0; i < data.length; i++) {
-                    if ((data[i].name.indexOf(keyword) !== -1) || (data[i].description.indexOf(keyword) !== -1)) {
-                        tempArray.push(data[i]);
+                if (keyword == '') { displayResults(data); } else {
+                    for (var i = 0; i < data.length; i++) {
+                        if ((data[i].name.indexOf(keyword) !== -1) || (data[i].description.indexOf(keyword) !== -1)) {
+                            tempArray.push(data[i]);
+                        }
                     }
+                    displayResults2(tempArray);
                 }
-
-                displayTable(tempArray);
-
             });
         }
 
@@ -71,7 +70,7 @@
             console.log(msg);
         }
 
-        function displayTable(param) {
+        function displayResults(param) {
             var $table = $("<table />");
             $table.attr("border", "1");  
             var $tbody = $("<tbody>");
@@ -91,19 +90,38 @@
             }
 
             $table.append($tbody);
-            
-            $('#results').append($table);
-
-            var results = $('#results');
-            results.empty();
-            results.append($table);
-
+            $('#allResults').append($table);
         }
+
+        function displayResults2(param) {
+            var $table = $("<table />");
+            $table.attr("border", "1");
+            var $tbody = $("<tbody>");
+            var $thead = $("<thead>");
+
+            $thead.append($("<tr>").html("<td>ID</td><td>Creator Name</td><td>Name</td><td>Description</td><td>Completed</td>"));
+            $table.append($thead);
+
+            for (var i = 0; i < param.length; i++) {
+                var $tr = $("<tr>");
+                $("<td />").text(param[i].id).appendTo($tr);
+                $("<td />").text(param[i].userName).appendTo($tr);
+                $("<td />").text(param[i].name).appendTo($tr);
+                $("<td />").text(param[i].description).appendTo($tr);
+                $("<td />").text(param[i].done).appendTo($tr);
+                $tbody.append($tr);
+            }
+
+            $table.append($tbody);
+
+            $('#filteredResults').empty();
+            $('#filteredResults').append($table);
+        }
+
 
         $(function () {
             $("#tabs").tabs();
         });
-
 
     });
 })();
