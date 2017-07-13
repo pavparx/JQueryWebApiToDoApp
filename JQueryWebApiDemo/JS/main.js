517fc2a3d3d8f8455cc6e2cb6f4357562edb2a60
Pavlos <pavlos@ascentsoftware.eu>
2017-07-13 10:07:16 +0200

    Tabs work properly.

--- JQueryWebApiDemo/JS/main.js
+++ JQueryWebApiDemo/JS/main.js
@@ -38,6 +38,10 @@
 
     $(document).ready(function () {
 
+        $(function () {
+            $("#tabs").tabs();
+        });
+
         $.when(getTasks(), getUsers())
             .then(function (tasksResponse, usersResponse) {
 
@@ -46,21 +50,27 @@
             }, queryFailed);
 
         function querySucceeded(data) {
-            
-            
+           
+
+            $('#allResults').append(displayResults(data));
+            $('#filteredResults').append(displayResults(data));
 
             $("#filterTasks").keyup(function () {
+
+                $("#tabs").tabs("option", "active", 1);
 
                 var tempArray = [];
 
                 var keyword = $(this).val();
-                if (keyword == '') { displayResults(data); } else {
+                if (!keyword) { $("#tabs").tabs("option", "active", 0);$('#filteredResults').empty(); $('#filteredResults').append(displayResults(data)); } else {
                     for (var i = 0; i < data.length; i++) {
                         if ((data[i].name.indexOf(keyword) !== -1) || (data[i].description.indexOf(keyword) !== -1)) {
                             tempArray.push(data[i]);
                         }
                     }
-                    displayResults2(tempArray);
+                    $('#filteredResults').empty();
+                    $('#filteredResults').append(displayResults(tempArray));
+                    
                 }
             });
         }
@@ -71,6 +81,7 @@
         }
 
         function displayResults(param) {
+
             var $table = $("<table />");
             $table.attr("border", "1");  
             var $tbody = $("<tbody>");
@@ -90,38 +101,10 @@
             }
 
             $table.append($tbody);
-            $('#allResults').append($table);
+            return $table;
         }
-
-        function displayResults2(param) {
-            var $table = $("<table />");
-            $table.attr("border", "1");
-            var $tbody = $("<tbody>");
-            var $thead = $("<thead>");
-
-            $thead.append($("<tr>").html("<td>ID</td><td>Creator Name</td><td>Name</td><td>Description</td><td>Completed</td>"));
-            $table.append($thead);
-
-            for (var i = 0; i < param.length; i++) {
-                var $tr = $("<tr>");
-                $("<td />").text(param[i].id).appendTo($tr);
-                $("<td />").text(param[i].userName).appendTo($tr);
-                $("<td />").text(param[i].name).appendTo($tr);
-                $("<td />").text(param[i].description).appendTo($tr);
-                $("<td />").text(param[i].done).appendTo($tr);
-                $tbody.append($tr);
-            }
-
-            $table.append($tbody);
-
-            $('#filteredResults').empty();
-            $('#filteredResults').append($table);
-        }
-
-
-        $(function () {
-            $("#tabs").tabs();
-        });
+      
+        
 
     });
 })();
